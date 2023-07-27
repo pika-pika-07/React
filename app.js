e210dd7c000763e6e092e189dfd85c05b171a554
parth bhatnagar <parthbhatnagar@192.168.1.10>
2023-07-27 08:41:48 +0530

    Initialised

---
+++ app.js
@@ -0,0 +1,20 @@
+const heading = React.createElement(
+  "h4",
+  { id: "heading" },
+  "Hello World from React"
+);
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+console.log(root);
+root.render(heading);
+
+const heading1 = React.createElement("h1", {}, "Yo");
+const heading2 = React.createElement("h2", {}, "Parth");
+
+const children = React.createElement("div", { id: "child" }, [
+  heading1,
+  heading2,
+]); // Passing it like array will have the elements as multiple children of parent at same level
+
+const parent = React.createElement("div", { id: "parent" }, children);
+//console.log(parent);
+//root.render(parent);
