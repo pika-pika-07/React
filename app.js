49cf8dfed4e111127f4a3b65ce30ff9a4defe7c4
parth bhatnagar <parthbhatnagar@192.168.1.10>
2023-07-29 00:02:19 +0530

    Removbed logs

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+const root = ReactDOM.createRoot(document.querySelector("#root")); // Creating a Root for React
+
 const heading = React.createElement(
   "h4",
-  { id: "heading" },
-  "Hello World from React"
+  { id: "heading" }, // Attributes to h4 React Element
+  "Hello World from React" // Content of h4 React Element
 );
-const root = ReactDOM.createRoot(document.querySelector("#root"));
-console.log(root);
-//root.render(heading);
 
-const heading1 = React.createElement("h1", {}, "Yo");
+const heading1 = React.createElement("h1", {}, "Yo"); // This is a react element which is an object
 const heading2 = React.createElement("h2", {}, "Parth1234");
 
 const children = React.createElement("div", { id: "child" }, [
+  heading,
   heading1,
   heading2,
 ]); // Passing it like array will have the elements as multiple children of parent at same level
 
 const parent = React.createElement("div", { id: "parent" }, children);
-//console.log(parent);
-root.render(parent);
+
+root.render(parent); // Rendering the element to DOm creates Reacxt Element to HTMl element
