b86deb07de4997e7497e50810386b5c7079a358a
parth bhatnagar <parthbhatnagar@parths-Mac-mini.local>
2023-07-29 15:57:59 +0530

    JSX

--- app.js
+++ app.js
@@ -22,12 +22,35 @@ const root = ReactDOM.createRoot(document.querySelector("#root")); // Creating a
 
 // JSX -> Its not html in JS. Its and HTML like syntax
 // JSX => Babel Transpiles to React.createElement => ReactElement - JS Object => HTML Element(render)
+// React Element
 const jsxHeading = (
   <h1 id="heading" className="headingClass">
-    Yo
+    React Element
   </h1>
 ); // Its a React Element. It is similar to heading1. Just another way of creating React Element
 // jsxHeading === heading1
 
+// React Component
+// Class based components
+// functional components
+
+// React Functional Component -> Its a function that returns a JSX code
+const HeadingComponent1 = () => {
+  return <h1> Heading </h1>;
+};
+
+const HeadingComponent = () => {
+  // Component Composition
+  return (
+    <div>
+      <HeadingComponent1 />
+      {HeadingComponent1()}
+      {jsxHeading}
+      <h2> React Component</h2>
+    </div>
+  );
+};
+
 //root.render(parent); // Rendering the element to DOm creates Reacxt Element to HTMl element
-root.render(jsxHeading);
+//root.render(jsxHeading);
+root.render(<HeadingComponent />);
