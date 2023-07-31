a9b09fbaf6ea9ead2f4a318b6acd4662308f3e57
parth bhatnagar <parthbhatnagar@192.168.1.10>
2023-07-31 22:06:53 +0530

    Made Card componentA

--- app.js
+++ app.js
@@ -1,56 +1,84 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-const root = ReactDOM.createRoot(document.querySelector("#root")); // Creating a Root for React
-
-// const heading = React.createElement(
-//   "h4",
-//   { id: "heading" }, // Attributes to h4 React Element
-//   "Hello World from React" // Content of h4 React Element
-// );
-
-// const heading1 = React.createElement("h1", {}, "Yo"); // This is a react element which is an object
-// const heading2 = React.createElement("h2", {}, "Parth1234");
-
-// const children = React.createElement("div", { id: "child" }, [
-//   heading,
-//   heading1,
-//   heading2,
-// ]); // Passing it like array will have the elements as multiple children of parent at same level
-
-//const parent = React.createElement("div", { id: "parent" }, children);
-
-// JSX -> Its not html in JS. Its and HTML like syntax
-// JSX => Babel Transpiles to React.createElement => ReactElement - JS Object => HTML Element(render)
-// React Element
-const jsxHeading = (
-  <h1 id="heading" className="headingClass">
-    React Element
-  </h1>
-); // Its a React Element. It is similar to heading1. Just another way of creating React Element
-// jsxHeading === heading1
-
-// React Component
-// Class based components
-// functional components
-
-// React Functional Component -> Its a function that returns a JSX code
-const HeadingComponent1 = () => {
-  return <h1> Heading </h1>;
-};
-
-const HeadingComponent = () => {
-  // Component Composition
+import { SampleData } from "./sampleData.js";
+/**
+ * Header
+ *  - Logo
+ *  - Nav Items
+ * Body
+ *  - Search
+ *  - Restraunt Container
+ *      - RestrauntCard
+ * Footer
+ *  - Copyright
+ *  - Links
+ *  - Address
+ *  - Contact
+ */
+const Header = () => {
   return (
-    <div>
-      <HeadingComponent1 />
-      {HeadingComponent1()}
-      {jsxHeading}
-      <h2> React Component</h2>
+    <div className="header">
+      <div className="logo">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2fvBlkZEE9-Z8j9CdrzplwYrpsggpfDZOkg&usqp=CAU" />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About us</li>
+          <li>Contact us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
     </div>
   );
 };
 
-//root.render(parent); // Rendering the element to DOm creates Reacxt Element to HTMl element
-//root.render(jsxHeading);
-root.render(<HeadingComponent />);
+const RestrauntCard = (props) => {
+  const { resData } = props;
+  const { cloudinaryImageId, name, cuisines, avgRating, costForTwo } =
+    resData?.info;
+  return (
+    <div className="res-card">
+      <img
+        src={`https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/${cloudinaryImageId}`}
+        className="res-logo"
+      />
+      <h4>{name}</h4>
+      <h4>{cuisines.join(",")}</h4>
+      <h4>{avgRating}</h4>
+      <h4>{costForTwo}</h4>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        {SampleData.map((data) => {
+          return (
+            <RestrauntCard
+              key={data.info.id}
+              resData={data}
+              resName="Macdonalds"
+              cuisine="Mexican, Asian"
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.querySelector("#root")); // Creating a Root for React
+
+root.render(<AppLayout />);
---
+++ notes/app.js
@@ -0,0 +1,72 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const root = ReactDOM.createRoot(document.querySelector("#root")); // Creating a Root for React
+
+// const heading = React.createElement(
+//   "h4",
+//   { id: "heading" }, // Attributes to h4 React Element
+//   "Hello World from React" // Content of h4 React Element
+// );
+
+// const heading1 = React.createElement("h1", {}, "Yo"); // This is a react element which is an object
+// const heading2 = React.createElement("h2", {}, "Parth1234");
+
+// const children = React.createElement("div", { id: "child" }, [
+//   heading,
+//   heading1,
+//   heading2,
+// ]); // Passing it like array will have the elements as multiple children of parent at same level
+
+//const parent = React.createElement("div", { id: "parent" }, children);
+
+// JSX -> Its not html in JS. Its and HTML like syntax
+// JSX => Babel Transpiles to React.createElement => ReactElement - JS Object => HTML Element(render)
+// React Element
+
+const jsxHeading = (
+  <h1 id="heading" className="headingClass">
+    React Element
+  </h1>
+); // Its a React Element. It is similar to heading1. Just another way of creating React Element
+// jsxHeading === heading1
+
+// React Component
+// Class based components
+// functional components
+
+// React Functional Component -> Its a function that returns a JSX code
+const HeadingComponent1 = () => {
+  return <h1> Heading </h1>;
+};
+
+const HeadingComponent = () => {
+  // Component Composition
+  return (
+    <div>
+      <HeadingComponent1 />
+      {HeadingComponent1()}
+      {jsxHeading}
+      <h2> React Component</h2>
+    </div>
+  );
+};
+
+//root.render(parent); // Rendering the element to DOm creates Reacxt Element to HTMl element
+//root.render(jsxHeading);
+root.render(<HeadingComponent />);
+
+/**
+ * Header
+ *  - Logo
+ *  - Nav Items
+ * Body
+ *  - Search
+ *  - Restraunt Container
+ *      - RestrauntCard
+ * Footer
+ *  - Copyright
+ *  - Links
+ *  - Address
+ *  - Contact
+ */
