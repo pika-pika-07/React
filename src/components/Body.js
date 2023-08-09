ab692b435becc85d8090116c18058d630553541d
parth bhatnagar <parthbhatnagar@192.168.1.10>
2023-08-09 16:06:54 +0530

    Integrated swiggy api

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,23 @@
-import { useState } from "react";
-import { SampleData } from "../utils/sampleData";
+import { useState, useEffect } from "react";
+
 import RestrauntCard from "./RestrauntCard";
 
 const Body = () => {
-  const [restraunts, setRestraunts] = useState(SampleData);
+  const [restraunts, setRestraunts] = useState([]);
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=30.7333148&lng=76.7794179&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    const res =
+      json.data.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
+    const resInfo = res.map((res) => res.info);
+    setRestraunts(resInfo);
+  };
   return (
     <div className="body">
       <div className="filter">
