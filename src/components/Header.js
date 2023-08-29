3745111add4427624d854a43267e04a59f6e509d
parth bhatnagar <parthbhatnagar@192.168.1.11>
2023-08-29 15:44:04 +0530

    Added online status icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,11 @@
 import { useState } from "react";
 import { LOGO_URL } from "../utils/constants";
 import { Link } from "react-router-dom";
+import useOnlineStatus from "../utils/useOnlineStatus";
+
 const Header = () => {
   const [btnName, setBtnName] = useState("Login");
+  const onlineStatus = useOnlineStatus();
   const onBtnClick = () => {
     if (btnName === "Login") {
       setBtnName("Logout");
@@ -17,6 +20,7 @@ const Header = () => {
       </div>
       <div className="nav-items">
         <ul>
+          <li>Online Status: {onlineStatus ? "🧑‍🎤" : "🧶"}</li>
           <li>
             {" "}
             <Link to="/">Home</Link>
