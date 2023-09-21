8dfeb0fb2c6f2e5228662fba26c3593da559ed5a
parth bhatnagar <parthbhatnagar@192.168.1.9>
2023-09-21 21:01:50 +0530

    Added contact component tests

--- src/components/__tests__/Contact.test.js
+++ src/components/__tests__/Contact.test.js
@@ -2,9 +2,42 @@ import { render, screen } from "@testing-library/react";
 import Contact from "../Contact";
 import "@testing-library/jest-dom";
 
-test("Should load contact us component", () => {
-  render(<Contact />); // will be rendered in jsdom
+describe("Contact us page Test cases", () => {
+  // we can write 'it' as well instead of 'test'
+  test("Should load contact us component", () => {
+    render(<Contact />); // will be rendered in jsdom
 
-  const heading = screen.getByRole("heading");
-  expect(heading).toBeInTheDocument();
+    const heading = screen.getByRole("heading");
+
+    // Assertion
+    expect(heading).toBeInTheDocument();
+  });
+
+  it("Should load button inside contact us component", () => {
+    render(<Contact />); // will be rendered in jsdom
+
+    const button = screen.getByText("Submit");
+
+    // Assertion
+    expect(button).toBeInTheDocument();
+  });
+
+  test("Should load input name inside contact us component", () => {
+    render(<Contact />); // will be rendered in jsdom
+
+    const inputName = screen.getByPlaceholderText("name");
+
+    // Assertion
+    expect(inputName).toBeInTheDocument();
+  });
+
+  test("Should load 2 input boxes on Contact Component", () => {
+    render(<Contact />); // will be rendered in jsdom
+
+    // Querying
+    const inputBoxes = screen.getAllByRole("textbox"); // Will return jsx element
+
+    // Assertion
+    expect(inputBoxes.length).toBe(2);
+  });
 });
