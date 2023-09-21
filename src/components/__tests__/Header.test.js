32a31b522a2479d92367f36bda9a309231c13f4f
parth bhatnagar <parthbhatnagar@192.168.1.9>
2023-09-21 21:16:01 +0530

    Added units tests for header

---
+++ src/components/__tests__/Header.test.js
@@ -0,0 +1,54 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import Header from "../Header";
+import "@testing-library/jest-dom";
+import { Provider } from "react-redux";
+import appStore from "../../utils/appStore";
+import { BrowserRouter } from "react-router-dom";
+
+import "@testing-library/jest-dom";
+
+it("should load Header component with a login button ", () => {
+  render(
+    <BrowserRouter>
+      <Provider store={appStore}>
+        <Header />
+      </Provider>
+    </BrowserRouter>
+  );
+
+  const loginButton = screen.getByRole("button");
+
+  expect(loginButton).toBeInTheDocument();
+});
+
+it("should load Header component with a cart items 0 ", () => {
+  render(
+    <BrowserRouter>
+      <Provider store={appStore}>
+        <Header />
+      </Provider>
+    </BrowserRouter>
+  );
+
+  const cart = screen.getByText("Cart (0)");
+  //const cart = screen.getByText(/Cart/); // we can use a regex too
+
+  expect(cart).toBeInTheDocument();
+});
+
+it("should change login button to logout on click ", () => {
+  render(
+    <BrowserRouter>
+      <Provider store={appStore}>
+        <Header />
+      </Provider>
+    </BrowserRouter>
+  );
+
+  const loginButton = screen.getByRole("button", { name: "Login" });
+  fireEvent.click(loginButton);
+
+  const logoutButton = screen.getByRole("button", { name: "Logout" });
+
+  expect(logoutButton).toBeInTheDocument();
+});
